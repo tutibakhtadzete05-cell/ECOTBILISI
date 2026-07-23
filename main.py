dca02085e40abb7c0ead831159bd82ee8a8a527e
tutibakhtadzete05-cell <tuti.bakhtadzete05@geolab.edu.ge>
2026-07-23 21:20:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -174,4 +174,4 @@ def delete_report(report_id):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=true)
+    app.run(host='0.0.0.0', port=port, debug=True)
